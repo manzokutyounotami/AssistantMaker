44d952866a42b3da24041605eba49ae67bae7646
naminodarie <gengesa@gmail.com>
2017-01-21 00:52:21 +0900

    タスクトレイ表示機能を追加

--- CortanaCommandServer/MainWindow.xaml.cs
+++ CortanaCommandServer/MainWindow.xaml.cs
@@ -12,6 +12,8 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using ToolStripMenuItem=System.Windows.Forms.ToolStripMenuItem;
+
 
 namespace CortanaCommandServer
 {
@@ -20,16 +22,64 @@ namespace CortanaCommandServer
     /// </summary>
     public partial class MainWindow : Window
     {
+        private NotifyIconWrapper notifyIconWrapper;
         public MainWindow()
         {
             InitializeComponent();
             this.DataContext = App.ViewModel;
+
+            if (App.ViewModel.Setting.IsMinimizeStart) {
+                this.WindowState = WindowState.Minimized;
+                ShowNotifyIcon();
+            }
+
             App.ViewModel.StartServerCommand.Execute(null);
             App.Current.Exit += (s, e) =>
             {
                 App.ViewModel.StopServerCommand.Execute(null);
             };
             
+        }
+
+        private void Window_Closing(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+            if (App.ViewModel.Setting.IsMinimizeOnClosing) {
+                e.Cancel = true;
+                this.WindowState = WindowState.Minimized;
+            }
+        }
+
+        private void Window_StateChanged(object sender, EventArgs e)
+        {
+            if (this.WindowState == WindowState.Minimized && App.ViewModel.Setting.UseTaskTray) {
+                ShowNotifyIcon();
+            }
+        }
+
+        private void ShowWindow(object sender = null, EventArgs e = null)
+        {
+            this.Show();
+            this.WindowState = WindowState.Normal;
+            notifyIconWrapper?.Dispose();
+        }
+
+        private void ShowNotifyIcon()
+        {
+            this.Hide();
+
+            notifyIconWrapper = new NotifyIconWrapper();
+            var info = Application.GetResourceStream(new Uri("pack://application:,,,/favicon.ico", UriKind.Absolute));
+            notifyIconWrapper.NotifyIcon.Icon = new System.Drawing.Icon(info.Stream);
+            notifyIconWrapper.NotifyIcon.DoubleClick += ShowWindow;
+
+            var toolStripMenuItemSetting = new ToolStripMenuItem("設定", null, ShowWindow);
+            var toolStripMenuItemExit = new ToolStripMenuItem("終了", null, (s, e) => App.Current.Shutdown());
+
+            notifyIconWrapper.ContextMenuStrip.Items.AddRange(new[]
+            {
+                    toolStripMenuItemSetting,
+                    toolStripMenuItemExit
+            });
         }
     }
 }
